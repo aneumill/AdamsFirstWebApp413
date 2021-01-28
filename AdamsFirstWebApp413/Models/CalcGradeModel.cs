a138a474995b0f1d2a0ba12ad1cbde338fc5df61
aneumill <71047690+aneumill@users.noreply.github.com>
2021-01-27 18:47:25 -0700

    Some comments for assignment #2

--- AdamsFirstWebApp413/Models/CalcGradeModel.cs
+++ AdamsFirstWebApp413/Models/CalcGradeModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+//Contains the models for the project. Retrieves values from the forms, also sets contraints (0-100 value must be entered) for the .js, that is displayed with the post method 
 namespace AdamsFirstWebApp413.Models
 {
     public class CalcGradeModel
